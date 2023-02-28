d0a75b5728e92fdd8235f5e844f7ed070b851632
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-28 22:11:26 +0300

    задача 66

---
+++ Task2/Task2.cs
@@ -0,0 +1,32 @@
+﻿int First= InputFigureFromConsole ("Введите первое число: ");
+int Second = InputFigureFromConsole ("Введите второе число: ");
+if (First == -1 | Second == -1)
+    { 
+    Console.WriteLine ("Число введено с ошибкой");
+    System.Environment.Exit (0);
+    }
+Console.Write ($"{ZumNaturalNumbers (First, Second)}");
+
+int InputFigureFromConsole (string invitation)
+{
+    Console.Write (invitation);
+    int i;
+    while (!int.TryParse (Console.ReadLine(), out i))
+    {
+        Console.WriteLine ("Ошибка ввода числа");
+        Console.Write (invitation);
+    }
+    if (i>=1) {return i;} else {return -1;}
+}
+
+int ZumNaturalNumbers (int num1, int num2)
+{
+    if (num1<=num2) 
+    {
+        return (num1 + ZumNaturalNumbers (num1+1, num2));
+    }
+    else
+    {
+        return 0;
+    }
+}
